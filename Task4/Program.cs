4fb467a6e5df32396c356765b3386702720ac646
HelixGen <138561148+HelixGen@users.noreply.github.com>
2024-02-01 21:22:57 +0400

    Add files via upload

---
+++ Task4/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int boardSize = 8;
+        int[] queensPositions = new int[boardSize];
+
+        Random randomGenerator = new Random();
+
+       
+        for (int i = 0; i < boardSize; i++)
+        {
+            queensPositions[i] = randomGenerator.Next(boardSize);
+        }
+
+       
+        while (!IsNonAttackingPlacement(queensPositions))
+        {
+            for (int i = 0; i < boardSize; i++)
+            {
+                queensPositions[i] = randomGenerator.Next(boardSize);
+            }
+        }
+
+       
+        Console.WriteLine("Randomly Placed Queens (0-based indexing):");
+        for (int i = 0; i < boardSize; i++)
+        {
+            for (int j = 0; j < boardSize; j++)
+            {
+                if (queensPositions[i] == j)
+                {
+                    Console.Write("Q ");
+                }
+                else
+                {
+                    Console.Write(". ");
+                }
+            }
+            Console.WriteLine();
+        }
+    }
+
+    static bool IsNonAttackingPlacement(int[] queensPositions)
+    {
+        for (int i = 0; i < queensPositions.Length; i++)
+        {
+            for (int j = i + 1; j < queensPositions.Length; j++)
+            {
+                if (queensPositions[i] == queensPositions[j] || Math.Abs(queensPositions[i] - queensPositions[j]) == Math.Abs(i - j))
+                {
+                    return false; 
+                }
+            }
+        }
+        return true; 
+    }
+}
